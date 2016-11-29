27caf346f71de1d2b4bb7ad6f57b029f0afb8f1c
Jan Rapp <janrapp@live.de>
2016-11-29 18:23:25 +0100

    Allow using definitions as shortcut when printing

--- src/lambda.ts
+++ src/lambda.ts
@@ -24,6 +24,95 @@ module lamdda {
     ) { }
   }
 
+  // a Map with custom hash and equals function
+  class Dict<K, V> {
+    map = new Map<string, [K, V][]>();
+    constructor(
+      private hash: (k: K) => string,
+      private equals: (k1: K, k2: K) => boolean,
+    ) { }
+    hintGet(key: K, hash: string): V | undefined {
+      let entries = this.map.get(hash);
+      if (typeof entries === "undefined") { return undefined; }
+      for (let e of entries) {
+        if (this.equals(key, e[0])) {
+          return e[1];
+        }
+      }
+      return undefined;
+    }
+    get(key: K): V | undefined {
+      return this.hintGet(key, this.hash(key));
+    }
+    set(key: K, value: V | undefined): [K, V] | undefined {
+      let hash = this.hash(key);
+      let entries = this.map.get(hash);
+      if (typeof entries === "undefined") {
+        if (typeof value !== "undefined") {
+          this.map.set(hash, [[key, value]]);
+        }
+        return undefined;
+      }
+      for (let i = 0; i < entries.length; i++) {
+        let e = entries[i];
+        if (this.equals(key, e[0])) {
+          if (typeof value === "undefined") {
+            entries.splice(i, 1);
+            return e;
+          }
+          entries[i] = [key, value];
+          return e;
+        }
+      }
+      if (typeof value !== "undefined") {
+        entries.push([key, value]);
+      }
+      return undefined;
+    }
+  }
+
+  // replace `definitions` in `e` for a more compact representation
+  function replaceDef(e: Exp, definitions: Map<string, Exp>): Exp {
+    let defs = new Dict<Exp, Var>(hash, equal);
+    for (let [varName, exp] of definitions) {
+      defs.set(exp, new Var(varName));
+    }
+    let [exp, expHash] = replace(e);
+    return tryReplace(exp, expHash);
+    function replace(e: Exp): [Exp, string] {
+      if (e.k === "Var") {
+        return [new Var(e.name), "v"];
+      } else if (e.k === "Apply") {
+        let [fun, funHash] = replace(e.fun);
+        let [arg, argHash] = replace(e.arg);
+        return [
+          new Apply(tryReplace(fun, funHash), tryReplace(arg, argHash)),
+          "(" + funHash + " " + argHash + ")",
+        ];
+      } else if (e.k === "Lambda") {
+        let [exp, expHash] = replace(e.exp);
+        return [
+          new Lambda(e.arg, tryReplace(exp, expHash)),
+          "(λ" + "v" + " " + expHash + ")",
+        ];
+      }
+      throw "invliad node in expression";
+    }
+    function tryReplace(e: Exp, hash: string) {
+      return defs.hintGet(e, hash) || e;
+    }
+    function hash(e: Exp): string {
+      if (e.k === "Var") {
+        return "v";
+      } else if (e.k === "Apply") {
+        return "(" + hash(e.fun) + " " + hash(e.arg) + ")";
+      } else if (e.k === "Lambda") {
+        return "(λ" + "v" + " " + hash(e.exp) + ")";
+      }
+      throw "invliad node in expression";
+    }
+  }
+
   function equal(a: Exp, b: Exp): boolean {
     if (a.k === "Var" && b.k === "Var") {
       return a.name === b.name;
@@ -72,6 +161,7 @@ module lamdda {
     throw "invliad node in expression";
   }
 
+  // pretty print
   function pprint(e: Exp): string {
     const ob = "([{<";
     const cb = ")]}>";
@@ -206,6 +296,8 @@ module lamdda {
 +1 = λa λf λn f (a f n); // next
 + = λa λb λf λn a f (b f n); // add
 * = λa λb λf λn a (b f) n; // mul
+^ = λa λb b (* a) 1; // exp
+kit^ = λa λb b a; // exp // should work (if b != 0) but doesn't (out of memory)
 
 6 = +1 5;
 7 = +1 6;
@@ -219,35 +311,73 @@ pair = λa λb λf f a b;
 tup = λp pair (+1 (1. p)) (1. p);
 -1 = λa 2. (a tup (pair 0 0)); // prev
 - = λa λb b -1 a; // sub
+
+T = λt λf t; // true
+F = λt λf f; // false // is the same as 0
+&& = λa λb a b F; // lazy or
+|| = λa λb a T b; // lazy and
+not = λa a F T;
+^^ = λa λb a (not b) b; // (not lazy (hehe)) xor
+
 Y = λf (λx f (x x)) (λx f (x x));
+isZero = λa a (λb F) T;
+fac = Y (λfac_ λn (isZero n) 1 (* n (fac_ (- n 1)))); // very slow when simplifing
+
 `;
     let defs = parseDef(definitions);
-    //for (let [v, e] of defs) {
-    //  out(v + " = " + pprint(e));
-    //}
-    //out(pprint(simplifyN(parse("pair 0 1", defs), 100)));
+    for (let [v, e] of defs) {
+      out(v + " = " + pprint(e));
+    }
+    //out(pprint(simplifyN(parse, ("pair 0 1", defs), 100)));
     //out(pprint(simplifyN(parse("pair 0 (+1 (+1 (+1 0)))", defs), 100)));
     //out(pprint(simplifyN(parse("2. (pair 0 1)", defs), 100)));
     //out(pprint(simplifyN(parse("(3 tup (pair 0 0))", defs), 100)));
     //out(pprint(simplifyN(parse("2. (3 tup (pair 0 0))", defs), 100)));
     //out(pprint(simplifyN(parse("-1 3", defs), 100)));
     //simplifyPrintSteps(parse("(5 tup (pair 0 0))", defs));
-    parseSimplifyPrint("- 7 9", defs);
-    parseSimplifyPrint("- 10 7", defs);
+    //parseSimplifyPrint("- 7 9", defs);
+    //parseSimplifyPrint("λa - 10 7", defs);
+    //parseSimplifyPrint("+ 5 5", defs);
+    //parseSimplifyPrint("* 3 3", defs);
+    //parseSimplifyPrint("^ 1 0", defs);
+    //parseSimplifyPrint("^ 1 3", defs);
+    //parseSimplifyPrint("^ 1 5", defs);
+    //parseSimplifyPrint("^ 2 0", defs);
+    //parseSimplifyPrint("^ 2 1", defs);
+    //parseSimplifyPrint("^ 2 2", defs);
+    //parseSimplifyPrint("^ 2 3", defs);
+    parseSimplifyPrint("&& T T", defs);
+    parseSimplifyPrint("&& T F", defs);
+    parseSimplifyPrint("&& F T", defs);
+    parseSimplifyPrint("&& F F", defs);
+    parseSimplifyPrint("|| T T", defs);
+    parseSimplifyPrint("|| T F", defs);
+    parseSimplifyPrint("|| F T", defs);
+    parseSimplifyPrint("|| F F", defs);
+    parseSimplifyPrint("not F", defs);
+    parseSimplifyPrint("not T", defs);
+    parseSimplifyPrint("^^ T T", defs);
+    parseSimplifyPrint("^^ T F", defs);
+    parseSimplifyPrint("^^ F T", defs);
+    parseSimplifyPrint("^^ F F", defs);
+    // count down to zero (from 5)
+    parseSimplifyPrint("Y (λf λa (isZero a) a (f (-1 a))) 5", defs);
+    parseSimplifyPrint("fac 3", defs);
     function parseSimplifyPrint(s: string, scope: Map<string, Exp>) {
-      out(pprint(simplify(parse(s, scope))));
+      out(s + " --> " + pprint(replaceDef(simplify(parse(s, scope)), scope)));
     }
-    function simplifyPrintSteps(e: Exp) {
-      let i = 1;
+    function simplifyPrintSteps(s: string, scope: Map<string, Exp>) {
+      let e = parse(s, scope);
+      let i = 0;
       out("init: " + pprint(e));
       while (true) {
         let newE = simplifyOnce(e);
+        out(i + ": " + pprint(newE));
+        out(i + "s:" + pprint(replaceDef(newE, scope)));
         if (equal(newE, e)) { break; }
         e = newE;
-        out(i + ": " + pprint(e));
         i++;
       }
-      out("final: " + pprint(e));
     }
   }
 }
